2388cfeeb05fd5d55176fbc9034d5bc9d6e30f86
Debojyoti Roy <debojyotiroy245@gmail.com>
2021-12-10 22:56:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-// Step 1 - selecting all the elements
+
 var initialPrice = document.querySelector("#initial-price");
 var stocksQuantity = document.querySelector("#stocks-quantity");
 var currentPrice = document.querySelector("#current-price");
 var submitBtn = document.querySelector("#submit-btn");
 var outputBox = document.querySelector("#output-box");
 
-// Ex-06
 submitBtn.addEventListener("click", submitHandler);
 
 function submitHandler() {
@@ -16,27 +15,26 @@ function submitHandler() {
   calculateProfitAndLoss(ip, qty, curr);
 }
 
-// Ex-05
 function calculateProfitAndLoss(initial, quantity, current) {
   if (initial > current) {
     var loss = (initial - current) * quantity;
     var lossPercentage = (loss / initial) * 100;
 
     showOutput(
-      `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
+      `Loss is ${loss} and the percent is ${lossPercentage}%`
     );
   } else if (current > initial) {
     var profit = (current - initial) * quantity;
     var profitPercentage = (profit / initial) * 100;
 
     showOutput(
-      `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
+      `Profit is ${profit} and the percent is ${profitPercentage}%`
     );
   } else {
-    showOutput(`No pain no gain and no gain no pain`);
+    showOutput(`No profit no loss`);
   }
 }
 
 function showOutput(message) {
-  outputBox.innerHTML = message;
+  outputBox.innerText = message;
 }
